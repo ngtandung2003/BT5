af7925b470721fdaddb8e498ee73af667f0163e7
ngtandung2003 <141381365+ngtandung2003@users.noreply.github.com>
2023-12-08 10:09:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import streamlit as st
+from PIL import Image
+import pickle as pkl
+import numpy as np
 
+
+st.title('Handwritten Number Detection')
+
+input = open ('lrc_mnist.pkl','rb')
+model = pkl.load(input)
+
+st.header('Upload an image')
+image = st.file_uploader('Choose an image', type=(['png','jpeg','jpg']))
+
+if image is not None:
+  image = Image.open(image)
+  st.image(image, caption='Test image')
+
+  if st.button('Predict'):
+    image = image.resize((28*28, 1))
+    vector = np.array(image)
+    label = str(model.predict(vector)[0])
+
+    st.header('Result')
+    st.text(label)
